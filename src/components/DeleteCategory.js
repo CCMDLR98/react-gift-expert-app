cb268c5700a4242a4f0001e599dea361f62f407c
CCMDLR <carlitos_1098_@hotmail.com>
2021-09-19 13:25:18 -0600

    first commit

---
+++ src/components/DeleteCategory.js
@@ -0,0 +1,7 @@
+
+    // const handleDelete = () =>{
+    //     const categoria = document.getElementById('categoria').value;
+    //     if ( categories.find(prevCategoria => prevCategoria != categoria) ) return alert('No existe esa categoria');
+    //     if ( categoria.length <=0 )return alert(' Insertar una Categoria ');
+    //     setCategories(prevState => [...prevState.filter(categori => categori != categoria)])
+    // }
